66da35782cab13ebbc0b31460e0700792fc04a6e
Tom O'Connell <tomoconnell@live.com>
2014-07-16 00:11:55 -0700

    Got rooms and lobby functioning as originally desired.

--- client/app/room/room.js
+++ client/app/room/room.js
@@ -2,7 +2,7 @@ angular.module('handleApp.room', [])
 
 .controller('roomController', function ($scope, EasyRTC) {
 	$scope.leaveRoom = EasyRTC.leaveRoom;
-	$scope.disconnect = EasyRTC.disconnect;
+	$scope.roomDisconnect = EasyRTC.roomDisconnect;
 	EasyRTC.interviewInit();
 });
 
