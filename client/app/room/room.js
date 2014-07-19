dc99018bb513b585f29103a6050d333716f7029b
Tom O'Connell <tomoconnell@live.com>
2014-07-19 14:08:32 -0700

    Merged client side work with deployment ready server work. Moved sign out button to right.

--- client/app/room/room.js
+++ client/app/room/room.js
@@ -1,15 +1,19 @@
 angular.module('handleApp.room', [])
 
-.controller('roomController', function ($scope, $window, $location, $rootScope, EasyRTC) {
+.controller('roomController', function ($scope, $window, $location, $rootScope, EasyRTC, InterviewQuestions) {
   $scope.signout = function () {
     $window.localStorage.removeItem('com.handle');
     $rootScope.userName = null;
     $location.path('/home');
   };
-
   $scope.currentRoom = EasyRTC.getCurrentRoom();
+	$scope.showQuestions = false;
+	$scope.shuffleQuestions = function () {
+    $scope.data.questions = $window._.shuffle($scope.data.questions); 
+	};
+	$scope.data = {};
 	$scope.leaveRoom = EasyRTC.leaveRoom;
-	$scope.roomDisconnect = EasyRTC.roomDisconnect;
+	$scope.data.questions = InterviewQuestions.questions;
 	EasyRTC.interviewInit();
 });
 
